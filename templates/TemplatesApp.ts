5f1e2048b091b2baf976fc6ad6293dfb7aca5aee
imvipindev <vipinchaudhary1809@gmail.com>
2023-03-15 23:13:46 +0530

    refactor(setup): :recycle: minor tweaks

--- templates/TemplatesApp.ts
+++ templates/TemplatesApp.ts
@@ -6,7 +6,7 @@ import {
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
-import { TemplatesCommand } from './commands/TemplatesCommand';
+import { TemplateCommand } from './commands/TemplateCommand';
 export class TemplatesApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
@@ -17,7 +17,8 @@ export class TemplatesApp extends App {
     public async extendConfiguration(
         configuration: IConfigurationExtend
       ): Promise<void> {
-        const helloWorldCommand: TemplatesCommand = new TemplatesCommand()
+        const helloWorldCommand: TemplateCommand = new 
+        TemplateCommand(this);
         await configuration.slashCommands.provideSlashCommand(helloWorldCommand)
       }
 }
