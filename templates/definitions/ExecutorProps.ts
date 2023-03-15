7e19a63e1fa4000f5a3022e6e7d502857914c93d
imvipindev <vipinchaudhary1809@gmail.com>
2023-03-15 23:15:12 +0530

    feat(interface): :sparkles: new interface for executorprops

---
+++ templates/definitions/ExecutorProps.ts
@@ -0,0 +1,18 @@
+import { IHttp, IModify, IPersistence, IRead } from "@rocket.chat/apps-engine/definition/accessors";
+import { IRoom } from "@rocket.chat/apps-engine/definition/rooms";
+import { SlashCommandContext } from "@rocket.chat/apps-engine/definition/slashcommands";
+import { IUser } from "@rocket.chat/apps-engine/definition/users";
+import { TemplatesApp } from "../TemplatesApp";
+
+export interface ExecutorProps {
+    bot: IUser;
+    sender: IUser;
+    room: IRoom;
+    command: string[];
+    context: SlashCommandContext;
+    read: IRead;
+    modify: IModify;
+    http: IHttp;
+    persistence: IPersistence;
+    app:TemplatesApp;
+}
